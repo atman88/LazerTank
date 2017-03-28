91aaf2b729e8698a719b2e699d2221a07c9ca156
Andrew Turcotte <turcotte.andy@gmail.com>
2017-03-28 09:54:27 -0400

    fix: future shot paths not always erasing

--- controller/movecontroller.cpp
+++ controller/movecontroller.cpp
@@ -151,6 +151,7 @@ void MoveController::wakeup()
                         break;
                     }
 
+                    mFutureShots.removePath( move );
                     mMoves.eraseFront();
                     move = mMoves.getFront();
                 }
